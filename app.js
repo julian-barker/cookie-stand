59e8fc90f8a1265fe0aef6eeba977997981dade7
Julian Barker <julian.c.barker@gmail.com>
2022-08-22 14:50:16 -0700

    add table, improve table styling, link b/w pages

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function $(x) {
+  return document.getElementById(x);
+}
+
 class Store {
   constructor (name, min, max, avgSales) {
     this.name = name;
@@ -39,10 +43,56 @@ class Store {
   // }
 }
 
+
+// const Seattle2 = {
+//   min: 4,
+//   max: 7,
+//   avg: 7,
+// };
+
 const Seattle = new Store('Seattle', 23, 65, 6.3);
 const Tokyo = new Store('Tokyo', 3, 24, 1.2);
 const Dubai = new Store('Dubai', 11, 38, 3.7);
 const Paris = new Store('Paris', 20, 38, 2.3);
 const Lima = new Store('Lima', 2, 16, 4.6);
 
+const stores = [Seattle, Tokyo, Dubai, Paris, Lima];
+
+function display() {
+  let i;
+  const tab = $('table');
+  let title = document.createElement('h2');
+  let headings = document.createElement('tr');
+  headings.innerHTML += '<th>Time</th>';
+  for (let store of stores){
+    headings.innerHTML += `<th>${store.name}</th>`;
+  }
+  tab.appendChild(headings);
+  for (i = 6; i < 20; i++) {
+    let t = i % 12;
+    let m = Math.floor(i/12) ? 'pm' : 'am';
+    let row = document.createElement('tr');
+    row.innerHTML += `<td>${t}${m}</td>`;
+    for (let store of stores){
+      let str = store.sim[i-6];
+      console.log(str);
+      let cookies = str.slice(str.search(': ') + 2);
+      console.log(cookies);
+      row.innerHTML += `<td>${cookies}</td>`;
+    }
+    tab.appendChild(row);
+  }
+  let totals = document.createElement('tr');
+  totals.innerHTML += '<td>Totals</td>';
+  for (let store of stores){
+    let str = store.sim[i-6];
+    console.log(str);
+    let cookies = str.slice(str.search(': ') + 2);
+    console.log(cookies);
+    totals.innerHTML += `<td>${cookies}</td>`;
+  }
+  tab.appendChild(totals);
+}
+
 console.log(Seattle, Tokyo, Dubai, Paris, Lima);
+display();
